035b2cbb5faa68eaac49c22fea621194b82bed6e
Oleg Shabashov <oleg.shabashov@gmail.com>
2016-12-25 11:08:52 +0700

    [+] Added shorten url, saving to DB and PJAX it back

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,12 +2,9 @@
 
 namespace app\controllers;
 
-use Yii;
-use yii\filters\AccessControl;
-use yii\web\Controller;
+use app\models\Url;
 use yii\filters\VerbFilter;
-use app\models\LoginForm;
-use app\models\ContactForm;
+use yii\web\Controller;
 
 class SiteController extends Controller
 {
@@ -17,21 +14,10 @@ class SiteController extends Controller
     public function behaviors()
     {
         return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'only' => ['logout'],
-                'rules' => [
-                    [
-                        'actions' => ['logout'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
             'verbs' => [
-                'class' => VerbFilter::className(),
+                'class'   => VerbFilter::className(),
                 'actions' => [
-                    'logout' => ['post'],
+                    'short' => ['post'],
                 ],
             ],
         ];
@@ -46,80 +32,28 @@ class SiteController extends Controller
             'error' => [
                 'class' => 'yii\web\ErrorAction',
             ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
         ];
     }
 
     /**
      * Displays homepage.
      *
-     * @return string
-     */
-    public function actionIndex()
-    {
-        return $this->render('index');
-    }
-
-    /**
-     * Login action.
+     * @param string $url
      *
      * @return string
      */
-    public function actionLogin()
+    public function actionIndex(string $url = null)
     {
-        if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
+        $model = new Url();
+
+        return $this->render('index', ['model' => $model]);
+    }
+
+    public function actionShort()
+    {
+        $model = new Url();
+        if ($model->load(\Yii::$app->request->post()) && $model->save()) {
+            return $this->renderAjax('_short', ['model' => $model]);
         }
-
-        $model = new LoginForm();
-        if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
-        }
-        return $this->render('login', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Logout action.
-     *
-     * @return string
-     */
-    public function actionLogout()
-    {
-        Yii::$app->user->logout();
-
-        return $this->goHome();
-    }
-
-    /**
-     * Displays contact page.
-     *
-     * @return string
-     */
-    public function actionContact()
-    {
-        $model = new ContactForm();
-        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
-            Yii::$app->session->setFlash('contactFormSubmitted');
-
-            return $this->refresh();
-        }
-        return $this->render('contact', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Displays about page.
-     *
-     * @return string
-     */
-    public function actionAbout()
-    {
-        return $this->render('about');
     }
 }
--- views/site/index.php
+++ views/site/index.php
@@ -1,53 +1,41 @@
 <?php
+use app\models\Url;
+use yii\bootstrap\ActiveForm;
+use yii\bootstrap\Html;
+use yii\widgets\Pjax;
 
-/* @var $this yii\web\View */
-
-$this->title = 'My Yii Application';
+/**
+ * @var yii\web\View $this
+ * @var Url          $model
+ */
 ?>
-<div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+<div class="jumbotron">
+    <?php
+    Pjax::begin([
+        'enablePushState' => false,
+        'timeout'         => false,
+    ]);
+    $form = ActiveForm::begin(
+        [
+            'action'  => ['site/short'],
+            'options' => [
+                'data-pjax' => true,
+            ],
+        ]
+    );
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+    echo $form->field($model, 'source')
+              ->textarea([
+                  'placeholder' => $model->getAttributeLabel('source'),
+                  'rows'        => 3,
+              ])
+              ->label(false);
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
+    echo Html::submitButton(Yii::t('app', 'Shorten me!'), ['class' => 'btn btn-success']);
 
-    <div class="body-content">
+    ActiveForm::end();
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
-    </div>
+    Pjax::end();
+    ?>
 </div>
